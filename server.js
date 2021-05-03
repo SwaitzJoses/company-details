ab00ce46c5279939c12987e3dbe804620f99d4fb
Swaitz Joses <swaitz.joses@gmail.com>
2021-05-03 20:42:20 +0530

    first commit

--- server.js
+++ server.js
@@ -26,7 +26,21 @@ app.use('/api/users',userRoute)
 
 app.use('/api/companies',companyRoute) 
 
+const __dirname = path.resolve()
 
+
+if ((process.env.NODE_ENV = 'production')) {
+  app.use(express.static(path.join(__dirname, '/companies/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'companies', 'build', 'index.html'))
+  )
+} 
+else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
 
 
   // 🔥  ✔️  🚀
